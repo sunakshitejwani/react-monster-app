051f1cb82c509f16aa92d39615b66c990fdad83d
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-26 00:31:16 +0530

    Introduce state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string : "Hello Warrior"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Sunakshi
-          </p>
+          <p>{this.state.string}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
