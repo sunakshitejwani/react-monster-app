18f76633548d720057a2200d4b3627a939da2c28
sunakshi96 <sunakshitejwani@gmail.com>
2020-03-30 00:15:04 +0530

    Filter state using destructuring, filter() and includes()

--- src/App.js
+++ src/App.js
@@ -18,18 +18,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
+    });
     return (
       <div className="App">
         <input
           type="search"
           placeholder="Search Monsters!"
-          onChange={e => {
-            this.setState({searchField:e.target.value},()=>{
-              console.log(this.state)
-            })
-          }}
+          onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
