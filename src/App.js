5c006891cfa0c58a3e56d55f3c020cc7c0f915ac
sunakshi96 <sunakshitejwani@gmail.com>
2020-03-31 02:12:51 +0530

    define Functional component Search-box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -24,10 +25,9 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="Search Monsters!"
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox 
+          placeholder = 'Search Monsters!'
+          handleChange = {e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
