83ace99f3187d698696a1965afc2cadf05490e3d
sunakshi96 <sunakshitejwani@gmail.com>
2020-04-01 02:38:02 +0530

    Updates

---
+++ precache-manifest.72e963caf9c66fe4369ffdb188c25af1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd0f37580a28f8fc806b9997afa8925",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "346654b49ebcc8f21dec",
+    "url": "/react-monster-app/static/css/main.3e5afb35.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/react-monster-app/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-monster-app/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346654b49ebcc8f21dec",
+    "url": "/react-monster-app/static/js/main.d853fc4b.chunk.js"
+  },
+  {
+    "revision": "1933dec57a330dac3377",
+    "url": "/react-monster-app/static/js/runtime-main.0bd94d00.js"
+  }
+]);
